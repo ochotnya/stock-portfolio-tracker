91bdb872d7f9e054017c3467b29947346822c0f9
adrian <ochotny.a@gmail.com>
2022-04-01 09:41:07 +0200

    changed React to v17. Installed materialUI. Removed boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+
+const styles = {
+  root: {
+    background: "blue",
+    border: "10px solid black",
+  },
+};
+
+const useStyles = makeStyles(styles);
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className={classes.root}>test</Button>
     </div>
   );
 }
